e59deed58cc67d71de2c4114062590c3ce590ad0
zhangbo <449625455@qq.com>
2020-03-23 21:16:57 +0800

    实现倒序单项链表

--- src/main/java/com/zhangbo/log/log4j/shujjiegou/SingleLinkedList.java
+++ src/main/java/com/zhangbo/log/log4j/shujjiegou/SingleLinkedList.java
@@ -2,12 +2,27 @@ package com.zhangbo.log.log4j.shujjiegou;
 
 import org.springframework.data.annotation.Transient;
 
+import java.util.Queue;
+import java.util.Stack;
+
 
 /**
  * @author zhangbo
  * ${Date} ${TIme}
  */
 public class SingleLinkedList {
+
+    public static void main(String[] args) {
+        SingleLinkedList list = new SingleLinkedList();
+        list.add(new DataNode(1,"1"));
+        list.add(new DataNode(2,"1"));
+        list.add(new DataNode(3,"1"));
+        list.add(new DataNode(4,"1"));
+        System.out.println(list.getRelase(2));
+//        list.reverse();
+        list.reverseByStack();
+        int c =0;
+    }
 
 
     private DataNode head;
@@ -95,6 +110,98 @@ public class SingleLinkedList {
         return size;
     }
 
+    /****
+     * 根据id来删除
+     *  单链表删除 找到上一个元素
+     * @param data
+     */
+    public void delete(DataNode data){
+        if (head.next == null){
+            throw new IndexOutOfBoundsException();
+        }
+        DataNode temp = head;
+        while (true){
+            if (temp.next == null){
+                break;
+            }
+            if (temp.next.id.equals(data.id)){ //找到了当前元素
+                //temp则为前面一个节点
+                temp.next = temp.next.next;
+                break;
+            }
+            temp = temp.next;
+        }
+    }
+
+    /****
+     * 查找单lian表的 倒数第k个元素
+     * @param k
+     * @return
+     */
+    public DataNode getRelase(int k){
+        if (head.next == null){
+            return null;
+        }
+        int result = size() - k;
+        if (result<0){
+            return null;
+        }
+        return get(result);
+    }
+
+    public void clear(){
+        head.next = null;
+    }
+
+    public void reverseByStack(){
+        if (head.next == null || head.next.next == null)
+            return;
+        Stack<DataNode> s = new Stack<>();
+        DataNode temp = head;
+        while (true){
+            if (temp.next == null){
+                break;
+            }
+            s.add(temp.next);
+            temp = temp.next;
+        }
+        clear();
+        DataNode revers = new DataNode(null,null);
+        while (s.size() >0){
+            revers.next = s.pop();
+            revers = revers.next;
+        }
+    }
+
+    /**
+     * 将原有节点反转 @linked: https://www.bilibili.com/video/av59600020?p=22
+     */
+    public void reverse(){
+        DataNode reverseNode = new DataNode(null,null);
+//        DataNode temp = head;
+//        DataNode temp1= null;
+//        while (true){
+//            if (temp.next == null){
+//                break;
+//            }
+//
+//            reverseNode.next = temp.next;
+//
+//            temp = temp.next;
+//        }
+
+        DataNode next = null;
+        DataNode cur = head.next;//当前的元素
+        while (cur != null) {
+            next = cur.next;
+            cur.next = reverseNode.next;
+            reverseNode.next = cur;
+            cur = next;
+        }
+        head.next = reverseNode.next;
+    }
+
+
 }
 
 class DataNode{
@@ -103,7 +210,6 @@ class DataNode{
 
     public String name;
 
-    @Transient
     public DataNode next;
 
     public DataNode(Integer id,String name){
